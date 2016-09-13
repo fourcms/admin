852d6b3819a7b00c513d6f0f2843a4a8570a1921
Avtandil Kikabidze <akalongman@gmail.com>
2016-09-13 12:33:32 +0400

    Fixes

--- src/Admin/Models/User.php
+++ src/Admin/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace FourCms\Admin\Models;
 
-use Itdc\Auth\RemoteUserTrait;
+use FourCms\Auth\RemoteUserTrait;
 use Longman\Platfourm\User\Models\Eloquent\User as BaseUser;
 
 class User extends BaseUser
