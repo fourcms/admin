10badcdbca40ca382faee086df275a44cd9e25dc
Avtandil Kikabidze <akalongman@gmail.com>
2016-09-13 15:17:48 +0400

    Updated platfourm version

--- src/Admin/Controllers/Api/HomeController.php
+++ src/Admin/Controllers/Api/HomeController.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of the FourCms Admin package.
+ *
+ * (c) Avtandil Kikabidze aka LONGMAN <akalongman@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace FourCms\Admin\Controllers\Api;
 
