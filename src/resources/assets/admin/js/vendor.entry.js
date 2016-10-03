9144f47ac96b82e640e9d1404ba1ad8d4e5fb791
thecotne <cotne@itdc.ge>
2016-10-03 12:50:51 +0400

    clean vendor.entry

--- src/resources/assets/admin/js/vendor.entry.js
+++ src/resources/assets/admin/js/vendor.entry.js
@@ -1,18 +1,9 @@
 require('babel-polyfill');
 window.$ = window.jQuery = require('jquery');
-// require('jquery-ui');
 require('bootstrap');
 require('admin-lte');
-// require('select2');
-// require('bootbox');
-// require('maskedinput');
-// window.moment = require('moment/moment.js');
-// require('imports?define=>false&exports=>false&require=>false!eonasdan-bootstrap-datetimepicker');
-// require('imports?define=>false&exports=>false&require=>false!daterangepicker');
-// import 'helpers/app';
 
 import {SENTRY_DSN} from 'helpers/env';
-
 
 window.raven = require('raven-js');
 
